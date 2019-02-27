43442b9e4d96b670886533ca7734bd4c92613148
fw867 <fw867@163.com>
2019-02-27 13:11:40 +0800

    update message for 2.28

--- fwupdate/push_message.json.js
+++ fwupdate/push_message.json.js
@@ -1,10 +1,13 @@
 {
-"title":"<font color='#FF0033'><b>Openwrt X64 V2.26 r9768 更新日志：</b></font>",
+"title":"<font color='#FF0033'><b>Openwrt X64 V2.28 r7705 更新日志：</b></font>",
 "content1":"<font color='#227700'>\
 <br/>注意：2.13以前版本固件升级到最新版本或者legacy BIOS固件升级到UEFI版固件会清除所有配置。升级前注意保存配置！<br/>\
-<br/>祝大家元旦快乐！一笔“狗”销，“猪”事顺利！",
+<br/>为减少软件中心插件维护工作量，从本版本开始以openwrt 18.06为底包开发。",
 "content2":"<font color='##cc338c'>\
-<b>1. </b>升级内核到4.14.90；<br />\
-<b>2. </b>增加docker支持；<br />\
+<b>1. </b>升级内核到4.14.103；<br />\
+<b>2. </b>增加mptcp支持；<br />\
+<b>2. </b>增加open-vm-tools；<br />\
+<b>2. </b>增加full cone开关（网络-防火墙）；<br />\
+<b>2. </b>删除管控功能，重写后将在软件中心以插件上线；<br />\
 <b>3. </b>修复一些bug；<br />",
 }
